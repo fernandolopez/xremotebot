bea932646fce11b3aa983d106ad6a97008c3f334
Fernando López <fernando.e.lopez@gmail.com>
2015-03-24 23:12:44 -0300

    Volviendo a python 2 por compatibilidad

--- remotebot/lib/message.py
+++ remotebot/lib/message.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import tornado.escape
 
 def value(value, msg_id=None):
--- tests/config_test.py
+++ tests/config_test.py
@@ -6,7 +6,7 @@ import io
 class ConfigTest(unittest.TestCase):
     def setUp(self):
         self.cfgstr = io.StringIO(
-            'secret = "{}"\n'.format('x' * 40)
+            u'secret = "{}"\n'.format('x' * 40)
         )
 
     def appendcfg(self, lines):
@@ -21,14 +21,14 @@ class ConfigTest(unittest.TestCase):
     def test_invalid_short_secret_should_raise_exception(self):
         with self.assertRaises(config.ConfigValueError):
             cfg = config.Config(
-                io.StringIO('secret = "CHANGE_ME"'))
+                io.StringIO(u'secret = "CHANGE_ME"'))
 
     def test_long_secret_should_be_ok(self):
         cfg = config.Config(
-                io.StringIO('secret = "{}"'.format('x' * 40)))
+                io.StringIO(u'secret = "{}"'.format('x' * 40)))
 
     def test_mode_should_be_valid(self):
-        self.appendcfg('mode = foobar\n')
+        self.appendcfg(u'mode = foobar\n')
         with self.assertRaises(config.ConfigValueError):
             cfg = config.Config(self.cfgstr)
 
@@ -37,7 +37,7 @@ class ConfigTest(unittest.TestCase):
         self.assertEqual('standalone', cfg['mode'])
 
     def test_comments_are_ignored(self):
-        self.appendcfg('#Comment\n# Best style\n## Still valid\n \t  # Blanks')
+        self.appendcfg(u'#Comment\n# Best style\n## Still valid\n \t  # Blanks')
 
 if __name__ == '__main__':
     unittest.main()
