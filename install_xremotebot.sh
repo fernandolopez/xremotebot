a7d4732ac25b17e63ada98e171a6585ef2fcbc3e
diego.fernandez <die.fernandez@gmail.com>
2016-10-19 00:33:16 -0300

    modificado script de instalacion

--- install_xremotebot.sh
+++ install_xremotebot.sh
@@ -1,24 +1,27 @@
-sudo apt-get update
+#/bin/bash
+WORKDIR=`dirname "$BASH_SOURCE"`
+apt-get update
 
 # General development essentials
-sudo apt-get install -y build-essential ssh git gitk
+apt-get install -y build-essential ssh git gitk
 
 # Sqlite
-sudo apt-get install -y sqlite
+apt-get install -y sqlite
 
 # Python development essentials
-sudo apt-get install -y python python-setuptools python-dev && sudo easy_install -U pip
-sudo apt-get install -y libxml2-dev libxslt-dev  # needed for Python package 'lxml'
+apt-get install -y python python-setuptools python-dev && sudo easy_install -U pip
+apt-get install -y libxml2-dev libxslt-dev  # needed for Python package 'lxml'
 
 # virtualenv, virtualenvwrapper stuff
-sudo pip install virtualenv virtualenvwrapper
+pip install virtualenv virtualenvwrapper
 echo "
 if [ -f /usr/local/bin/virtualenvwrapper.sh ] ; then
   . /usr/local/bin/virtualenvwrapper.sh
 fi
 export WORKON_HOME=~/Envs
 " >> $HOME/.bashrc
-. $HOME/.bashrc
+PS1='$ '
+source $HOME/.bashrc
 mkdir -p $WORKON_HOME
 
 # Add personal bin to path
@@ -30,12 +33,17 @@ fi
 ' >> $HOME/.bashrc
 
 ## XRemoteBot
-git clone https://github.com/fernandolopez/xremotebot.git
+git clone https://github.com/fernandolopez/xremotebot.git xremotebot
 cd xremotebot
-virtualenv .
-. bin/activate
+virtualenv $PWD
+echo $PWD
+source bin/activate
 pip install -r requirements.txt
 pip install git+https://github.com/Robots-Linti/duinobot.git
 pip install git+https://github.com/fernandolopez/Myro.git
-sudo apt-get install libav-tools nodejs 
-npm install ws
+#streaming
+add-apt-repository ppa:mc3man/trusty-media
+apt-get update
+apt-get dist-upgrade
+apt-get install ffmpeg
+
