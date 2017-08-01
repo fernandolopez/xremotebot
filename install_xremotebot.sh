7850ec6edea0317fa306d1831cc3479f8f2e41de
Fernando López <fernando.e.lopez@gmail.com>
2017-08-01 15:52:10 -0300

    Script de instalación modificado

--- install_xremotebot.sh
+++ install_xremotebot.sh
@@ -1,49 +1,28 @@
 #/bin/bash
-WORKDIR=`dirname "$BASH_SOURCE"`
+set -e
 apt-get update
 
 # General development essentials
-apt-get install -y build-essential ssh git gitk
+apt-get install -y build-essential git
 
 # Sqlite
 apt-get install -y sqlite
 
 # Python development essentials
-apt-get install -y python python-setuptools python-dev && sudo easy_install -U pip
+apt-get install -y python python-setuptools python-dev
+apt-get install -y python-pip
 apt-get install -y libxml2-dev libxslt-dev  # needed for Python package 'lxml'
 
-# virtualenv, virtualenvwrapper stuff
-pip install virtualenv virtualenvwrapper
-echo "
-if [ -f /usr/local/bin/virtualenvwrapper.sh ] ; then
-  . /usr/local/bin/virtualenvwrapper.sh
-fi
-export WORKON_HOME=~/Envs
-" >> $HOME/.bashrc
-PS1='$ '
-source $HOME/.bashrc
-mkdir -p $WORKON_HOME
-
-# Add personal bin to path
-echo '
-# Add personal bin to PATH
-if [ -d "$HOME/bin" ]; then
-  PATH="$HOME/bin:$PATH"
-fi
-' >> $HOME/.bashrc
+# virtualenv
+pip install virtualenv
 
 ## XRemoteBot
-git clone https://github.com/dropsy-unlp/xremotebot.git xremotebot
-cd xremotebot
 virtualenv $PWD
 echo $PWD
 source bin/activate
 pip install -r requirements.txt
 pip install git+https://github.com/Robots-Linti/duinobot.git
-pip install git+https://github.com/fernandolopez/Myro.git
+#pip install git+https://github.com/fernandolopez/Myro.git
 #streaming
-add-apt-repository ppa:mc3man/trusty-media
-apt-get update
-apt-get dist-upgrade
-apt-get install ffmpeg
+apt-get -y install ffmpeg
 
